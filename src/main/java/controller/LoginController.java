447fd981e2cb8b4096cdd72547d70da1876d08af
mbalcer <mateusz75319@gmail.com>
2019-04-08 17:54:30 +0200

    Added show password service

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,4 +1,62 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    @FXML
+    private TextField tf_l_login;
+
+    @FXML
+    private PasswordField pf_l_password;
+
+    @FXML
+    private Button btn_sign_in;
+
+    @FXML
+    private CheckBox cb_show;
+
+    @FXML
+    private TextField tf_r_login;
+
+    @FXML
+    private TextField tf_r_email;
+
+    @FXML
+    private PasswordField pf_r_password;
+
+    @FXML
+    private PasswordField pf_r_repeatPassword;
+
+    @FXML
+    private Button btn_sign_up;
+
+    private String password;
+
+    @FXML
+    void showPassword() {
+        if (cb_show.isSelected()) {
+            password = pf_l_password.getText();
+            pf_l_password.clear();
+            pf_l_password.setPromptText(password);
+        } else {
+            pf_l_password.setText(password);
+            pf_l_password.setPromptText("");
+        }
+    }
+
+    @FXML
+    void signIn() {
+
+    }
+
+    @FXML
+    void signUp() {
+
+    }
+
 }
