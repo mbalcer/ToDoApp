c4e511b9a5d412a25ece786f4bd2a44173956307
mbalcer <mateusz75319@gmail.com>
2019-04-09 22:17:31 +0200

    Corrected hibernate configuration

--- src/main/java/database/HibernateFactory.java
+++ src/main/java/database/HibernateFactory.java
@@ -9,6 +9,7 @@ public class HibernateFactory {
     public SessionFactory getSessionFactory() {
         Configuration configuration = new Configuration().configure();
         configuration.addAnnotatedClass(entity.User.class);
+        configuration.addAnnotatedClass(entity.Task.class);
         StandardServiceRegistryBuilder registryBuilder =
                 new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
         SessionFactory sessionFactory = configuration.buildSessionFactory(registryBuilder.build());
