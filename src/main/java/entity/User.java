53e2ba8c022987e42916765dcc32724522e7b050
mbalcer <mateusz75319@gmail.com>
2019-04-09 18:44:13 +0200

    Little corrections

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     private String login;
     private String password;
