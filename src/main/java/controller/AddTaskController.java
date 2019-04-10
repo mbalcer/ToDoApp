e3789a5590f279ae56507071caaf12aea6cfd8ba
mbalcer <mateusz75319@gmail.com>
2019-04-10 22:22:44 +0200

    Created simple add task script

--- src/main/java/controller/AddTaskController.java
+++ src/main/java/controller/AddTaskController.java
@@ -3,10 +3,16 @@ package controller;
 import com.jfoenix.controls.JFXColorPicker;
 import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTimePicker;
+import dao.TaskDAO;
+import entity.Task;
 import entity.User;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Date;
 
 public class AddTaskController {
 
@@ -26,6 +32,7 @@ public class AddTaskController {
     private JFXColorPicker colorTask;
 
     private LoginController loginController;
+    private TaskDAO taskDAO;
     private User user;
 
     public void setLoginController(LoginController loginController) {
@@ -36,9 +43,16 @@ public class AddTaskController {
         this.user = user;
     }
 
+    public void initialize() {
+        taskDAO = new TaskDAO();
+    }
+
     @FXML
     void addTask() {
-
+        Instant instant = Instant.from(dateTask.getValue().atStartOfDay(ZoneId.systemDefault()));
+        Date date = Date.from(instant);
+        Task task = new Task(user.getId(), nameTask.getText(), date, descriptionTask.getText(), colorTask.getValue().toString(), false);
+        taskDAO.add(task);
     }
 
     @FXML
--- src/main/java/dao/TaskDAO.java
+++ src/main/java/dao/TaskDAO.java
@@ -3,12 +3,28 @@ package dao;
 import database.HibernateFactory;
 import entity.Task;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TaskDAO {
+    public void add(Task task) {
+        HibernateFactory hibernateFactory = new HibernateFactory();
+        Session session = hibernateFactory.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            session.save(task);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
 
     public List<Task> readAll(Long userId) {
         HibernateFactory hibernateFactory = new HibernateFactory();
